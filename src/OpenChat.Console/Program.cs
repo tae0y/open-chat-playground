a4f80bc79cec6e6956f6041d699b41add7584da8
tae0y[박태영] <tae0yp@gmail.com>
2025-09-07 19:36:19 +0900

    Update Consoel Progrom
    - Add pulling models logic
    - Check if local model is available
    - Problem: There is exaone model in host, but program cannot use it. it pulls everytime.

--- src/OpenChat.Console/Program.cs
+++ src/OpenChat.Console/Program.cs
@@ -13,8 +13,26 @@ class Program
             Uri = new Uri("http://localhost:11434"),
             Model = "hf.co/LGAI-EXAONE/EXAONE-4.0-1.2B-GGUF"
         };
-
         var chatClient = new OllamaApiClient(config);
+
+        var models = await chatClient.ListLocalModelsAsync();
+        foreach (var model in models)
+        {
+            Console.WriteLine($"Model: {model.Name}, Size: {model.Size}, Full: {model}");
+        }
+
+        if (models.Any(m => m.Name == config.Model))
+        {
+            Console.WriteLine($"Model {config.Model} is available.");
+        }
+        else
+        {
+            Console.WriteLine($"Model {config.Model} is not available. Please pull the model first.");
+            await foreach (var status in chatClient.PullModelAsync(config.Model))
+            {
+                Console.WriteLine($"{status?.Percent}% {status?.Status}");
+            }
+        }
 
         var messages = new ChatMessage(ChatRole.User, "밤하늘은 무슨 색인가요?");
         var chatOptions = new ChatOptions();
