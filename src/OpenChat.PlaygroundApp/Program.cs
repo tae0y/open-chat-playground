12eb7b1ca28ab75ad9439de2b145d6e1d00c9129
Justin Yoo <justin.yoojh@gmail.com>
2025-10-05 00:33:40 +0900

    Define endpoint (#486)
    * Define endpoint
    
    * Update README
    
    * Add the Required attribute for each property.

--- src/OpenChat.PlaygroundApp/Program.cs
+++ src/OpenChat.PlaygroundApp/Program.cs
@@ -1,4 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
+using Microsoft.AspNetCore.OpenApi;
 using Microsoft.Extensions.AI;
+using Microsoft.OpenApi.Models;
 
 using OpenChat.PlaygroundApp.Abstractions;
 using OpenChat.PlaygroundApp.Components;
@@ -22,6 +26,14 @@ builder.Services.AddChatClient(chatClient)
                 .UseFunctionInvocation()
                 .UseLogging();
 
+// 👇👇👇 OpenAPI - TO BE REFACTORED
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddOpenApi("openapi", options =>
+{
+    options.AddDocumentTransformer<OpenApiDocumentTransformer>();
+});
+// 👆👆👆 OpenAPI - TO BE REFACTORED
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,14 +42,103 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+
+    app.UseHttpsRedirection();
 }
 
-app.UseHttpsRedirection();
 app.UseAntiforgery();
-
 app.UseStaticFiles();
+
+// 👇👇👇 OpenAPI - TO BE REFACTORED
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi("/{documentName}.json");
+}
+
+var group = app.MapGroup("/api");
+group.MapPost("/chat/responses", (IEnumerable<ChatRequest> request) =>
+{
+    return new List<ChatResponse>
+    {
+        new() { Message = "This " },
+        new() { Message = "is " },
+        new() { Message = "a " },
+        new() { Message = "placeholder " },
+        new() { Message = "response " },
+        new() { Message = "from " },
+        new() { Message = "the " },
+        new() { Message = "chat " },
+        new() { Message = "API." },
+    };
+})
+.WithTags("Chat")
+.Accepts<IEnumerable<ChatRequest>>(contentType: "application/json")
+.Produces<List<ChatResponse>>(statusCode: StatusCodes.Status200OK, contentType: "application/json")
+.WithName("PostChatResponses")
+.WithOpenApi();
+// 👆👆👆 OpenAPI - TO BE REFACTORED
 
 app.MapRazorComponents<App>()
    .AddInteractiveServerRenderMode();
 
 await app.RunAsync();
+
+// 👇👇👇 OpenAPI - TO BE REFACTORED
+/// <summary>
+/// This represents the chat request entity.
+/// </summary>
+public class ChatRequest
+{
+    /// <summary>
+    /// Gets or sets the role of the message sender.
+    /// </summary>
+    [Required]
+    public string Role { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the message content.
+    /// </summary>
+    [Required]
+    public string Message { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// This represents the chat response entity.
+/// </summary>
+public class ChatResponse
+{
+    /// <summary>
+    /// Gets or sets the message content.
+    /// </summary>
+    [Required]
+    public string Message { get; set; } = string.Empty;
+}
+
+/// <summary>
+/// This represents the transformer entity for OpenAPI document.
+/// </summary>
+public class OpenApiDocumentTransformer(IHttpContextAccessor accessor) : IOpenApiDocumentTransformer
+{
+    /// <inheritdoc />
+    public Task TransformAsync(OpenApiDocument document, OpenApiDocumentTransformerContext context, CancellationToken cancellationToken)
+    {
+        document.Info = new OpenApiInfo
+        {
+            Title = "OpenChat Playground API",
+            Version = "1.0.0",
+            Description = "An API for the OpenChat Playground."
+        };
+        document.Servers =
+        [
+            new OpenApiServer
+            {
+                Url = accessor.HttpContext != null
+                    ? $"{accessor.HttpContext.Request.Scheme}://{accessor.HttpContext.Request.Host}/"
+                    : "http://localhost:5280/"
+            }
+        ];
+
+        return Task.CompletedTask;
+    }
+}
+// 👆👆👆 OpenAPI - TO BE REFACTORED
